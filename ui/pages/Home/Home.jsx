11ba725146f2f7687b152f5dcbfbbb0746a4d2a6
Benjamin Fazli <bfzli@hotmail.com>
2022-10-30 23:42:24 +0100

    bfzli:fix

--- ui/pages/Home/Home.jsx
+++ ui/pages/Home/Home.jsx
@@ -143,8 +143,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="bg-gray-50">
-      <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
+      <div style={{marginBottom: '1em'}} className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           <span className="block">Bamirësia nuk e pakëson pasurinë</span>
           <span className="block text-indigo-600">Jep Fal ndihmo komunitetin</span>
@@ -169,7 +168,6 @@ export default function Home() {
               Merr Falas
             </a>
             </Link>
-          </div>
         </div>
       </div>
     </div>
