ded4e1aa7fa0b348381e66c8ba0cd8f199b8e652
Benjamin Fazli <bfzli@hotmail.com>
2022-11-28 13:40:38 +0100

    bfzli:add

--- ui/components/Pagination/Pagination.jsx
+++ ui/components/Pagination/Pagination.jsx
@@ -18,13 +18,8 @@ export default function Pagination({ nPages, currentPage, setCurrentPage }) {
       <div class='flex justify-center m-10'>
         <nav>
           <ul class='flex list-style-none'>
-            <li class='page-item'>
-              <a
-                class='mr-2 page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none'
-                style={currentPage === 1 ? { display: 'none' } : {}}
-                onClick={prevPage}>
+            <li onClick={prevPage} style={currentPage === 1 ? { display: 'none' } : {}} class='mr-2 page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none cursor-pointer'>
                 E mëparshmja
-              </a>
             </li>
 
             {pageNumbers.map((pgNumber) => (
@@ -33,21 +28,15 @@ export default function Pagination({ nPages, currentPage, setCurrentPage }) {
                 key={pgNumber}
                 class={
                     currentPage === pgNumber
-                    ? 'page-link relative block text-gray-800 focus:shadow-none bg-gray-200 page-item w-10 h-10 flex justify-center items-center rounded border-0 cursor-pointer bg-transparent outline-none transition-all duration-300 cursor-pointer mr-2'
-                    : 'page-link relative block text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none page-item w-10 h-10 flex justify-center items-center rounded border-0 cursor-pointer bg-transparent outline-none transition-all duration-300 cursor-pointer mr-2' 
+                    ? 'page-link relative block text-gray-800 focus:shadow-none bg-gray-200 page-item w-9 h-9 flex justify-center items-center rounded border-0 cursor-pointer bg-transparent outline-none transition-all duration-300 cursor-pointer mr-2'
+                    : 'page-link relative block text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none page-item w-9 h-9 flex justify-center items-center rounded border-0 cursor-pointer bg-transparent outline-none transition-all duration-300 cursor-pointer mr-2' 
                 }>
                   {pgNumber}
               </li>
             ))}
 
-            <li class='page-item'>
-              <a
-                style={currentPage === nPages ? { display: 'none' } : {}}
-                onClick={nextPage}
-                class='page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none'
-                >
+            <li class='page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none cursor-pointer' onClick={nextPage} style={currentPage === nPages ? { display: 'none' } : {}}>
                 Tjetra
-              </a>
             </li>
           </ul>
         </nav>
