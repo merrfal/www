6134043dd6d5d2841e90f8551685884c241f22d4
Benjamin Fazli <bfzli@hotmail.com>
2022-11-28 13:29:44 +0100

    bfzli:add | adding meta tags and other general infos

--- data/metas/index.jsx
+++ data/metas/index.jsx
@@ -1,6 +1,5 @@
 import Terms from './Terms';
 import Privacy from './Privacy';
-import About from './About';
 import Error from './Error';
 import Home from './Home';
 import Loader from './Loader';
@@ -15,7 +14,6 @@ import UserSaves from './UserSaves';
 export {
   Terms,
   Privacy,
-  About,
   Error,
   Home,
   Loader,
@@ -27,3 +25,19 @@ export {
   UserProducts,
   UserSaves
 }
+
+// a po i sheh qito meta tags?
+// qito perdoren per me qit titullin e faqes
+// apo edhe desccription ne google search ose searches tjera
+// Amazon.com. Spend less. Smile more. qikjo osht titulli
+// qikjo osht description i amazon;
+// Free shipping on millions of items. Get the best of Shopping and Entertainment with Prime. Enjoy low prices and great deals on the largest selection of ...
+// e qellimi osht shtash me i bo na per secilen page qe kur ta publikojna te shfqet qysh duhet okej?
+
+// e tash un qito i kom kriu per sicilen faqe
+
+// edhe e kom qit ka njo default dhe i kom bo import te scila faqe te njejta
+// e tash duhesh me i ndryshu per secilen qe me u shfaq si duhet
+// un e kom kry veq per Home e ty qito tjerat kejt, kontentin e mendon vet 
+
+// psh nese osht edit produkti e qet titullin: Editimi i produktit - Merr Fal
--- ui/components/Pagination/Pagination.jsx
+++ ui/components/Pagination/Pagination.jsx
@@ -1,51 +1,57 @@
 export default function Pagination({ nPages, currentPage, setCurrentPage }) {
+  let pageNumbers = [...Array(0).keys()].slice(1);
 
-    let pageNumbers = [...Array(0).keys()].slice(1)
+  if (nPages) pageNumbers = [...Array(nPages + 1).keys()].slice(1);
 
-    if (nPages) {
-        pageNumbers = [...Array(nPages + 1).keys()].slice(1)
-    }
+  const nextPage = () => {
+    if (currentPage !== nPages) setCurrentPage(currentPage + 1);
+  };
 
-    const nextPage = () => {
-        if (currentPage !== nPages)
-            setCurrentPage(currentPage + 1)
-    }
+  const prevPage = () => {
+    if (currentPage !== 1) setCurrentPage(currentPage - 1);
+  };
 
-    const prevPage = () => {
-        if (currentPage !== 1)
-            setCurrentPage(currentPage - 1)
-    }
-    
-    return (
-        nPages >= 2 && (<div class="flex justify-center m-10">
-            <nav aria-label="Page navigation example">
-                <ul class="flex list-style-none">
-                    <li class="page-item"><a
-                        class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-                        style={currentPage === 1 ? { display: 'none' } : {}}
-                        onClick={prevPage}
+  console.log('data', nPages, currentPage);
 
-                        href="#">Previous</a></li>
+  return (
+    nPages >= 2 && (
+      <div class='flex justify-center m-10'>
+        <nav>
+          <ul class='flex list-style-none'>
+            <li class='page-item'>
+              <a
+                class='mr-2 page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none'
+                style={currentPage === 1 ? { display: 'none' } : {}}
+                onClick={prevPage}>
+                E mëparshmja
+              </a>
+            </li>
 
-                    {
-                        pageNumbers.map(pgNumber => (
-                            <li key={pgNumber} class="page-item">
-                                <a
-                                    class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-                                    onClick={() => setCurrentPage(pgNumber)}> {pgNumber}</a>
-                            </li>
-                        ))
-                    }
+            {pageNumbers.map((pgNumber) => (
+              <li
+                onClick={() => setCurrentPage(pgNumber)}
+                key={pgNumber}
+                class={
+                    currentPage === pgNumber
+                    ? 'page-link relative block text-gray-800 focus:shadow-none bg-gray-200 page-item w-10 h-10 flex justify-center items-center rounded border-0 cursor-pointer bg-transparent outline-none transition-all duration-300 cursor-pointer mr-2'
+                    : 'page-link relative block text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none page-item w-10 h-10 flex justify-center items-center rounded border-0 cursor-pointer bg-transparent outline-none transition-all duration-300 cursor-pointer mr-2' 
+                }>
+                  {pgNumber}
+              </li>
+            ))}
 
-
-                    <li class="page-item"><a
-                        style={currentPage === nPages ? { display: 'none' } : {}}
-                        onClick={nextPage}
-                        class="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-                        href="#">Next</a></li>
-                </ul>
-            </nav>
-        </div>
-        )
+            <li class='page-item'>
+              <a
+                style={currentPage === nPages ? { display: 'none' } : {}}
+                onClick={nextPage}
+                class='page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none'
+                >
+                Tjetra
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     )
+  );
 }
