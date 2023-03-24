c909f99c7635cd9766105e5de4be10d198ffa48e
Dren Bytyqi <db47104@ubt-uni.net>
2023-03-24 11:20:57 +0100

    Product Page responsive for mobile

--- ui/pages/Product/Thumbnail.jsx
+++ ui/pages/Product/Thumbnail.jsx
@@ -5,7 +5,8 @@ export default function Thumbnail({ gallery, index }) {
         <img
           src={gallery[index]}
           alt="Product Thumbnail"
-          className="w-full h-[50vh] object-center object-cover sm:rounded-lg"
+          className="w-full h-auto object-center object-cover rounded-lg"
+          style={{ maxHeight: "50vh" }}
         />
       </div>
     </div>
