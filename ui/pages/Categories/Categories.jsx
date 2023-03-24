43e89a7cfaf08149ac69f7cd4be17987c818ae12
Flaka <flaka.kallabaa@gmail.com>
2023-03-24 13:30:58 +0100

    Category

--- ui/pages/Categories/Categories.jsx
+++ ui/pages/Categories/Categories.jsx
@@ -7,21 +7,23 @@ export default function Categories() {
   return (
     <Normal>
       <Header />
+
       <div className="py-3 sm:py8 xl:max-w-7xl xl:mx-auto xl:px-8">
         <div className="mt-4 flow-root">
           <div className="-my-2">
-            <div className="box-content py-2 relative overflow-x-auto xl:overflow-visible">
-              <div className="h-[400px] absolute min-w-screen-xl px-4 flex space-x-8 sm:px-6 lg:px-8 xl:relative xl:px-0 xl:space-x-0 xl:grid xl:grid-cols-4 xl:gap-8">
-                {AllCategories.map((c) => (
-                  <Category category={c} key={c._id} />
-                ))}
-              </div>
-             <End />
+            <div className="box-content py-2 relative overflow-visible">
+           { // relative min-w-screen-xl px-4 flex space-x-8 sm:px-6 lg:px-8 xl:relative px-0 xl:space-x-0 grid grid-cols-1 xl:grid xl:grid-cols-4 xl:gap-8
+          }
+            <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+                    {AllCategories.map((c) => (
+                      <Category category={c} key={c._id} kategori={true}/>
+                    ))}
+                  </div>
+                 <End />
             </div>
           </div>
         </div>
       </div>
-      
     </Normal>
   );
 }
