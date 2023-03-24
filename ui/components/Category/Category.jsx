156fdc3de869accc991d56a7130cabafd765ec7a
Flaka <flaka.kallabaa@gmail.com>
2023-03-24 13:35:31 +0100

    border

--- ui/components/Category/Category.jsx
+++ ui/components/Category/Category.jsx
@@ -6,7 +6,7 @@ export default function Category({ category, index, kategori }) {
   return (
     <Link href={`/kategoria/${slug}`} key={index}>
 
-      <a className={kategori ? "hover:cursor-pointer relative h-[260px] sm:h-[400px] mb-6 rounded-lg rounded p-6 flex flex-col mx-2 transition-all" :
+      <a className={kategori ? "hover:cursor-pointer relative h-[260px] sm:h-[400px] mb-6 rounded-lg rounded-xl rounded p-6 flex flex-col mx-2 transition-all" :
         "hover:cursor-pointer relative h-[350px] sm:h-[400px] mb-6 rounded-lg rounded p-6 flex flex-col mx-2 transition-all"
       }>
         <span className="absolute inset-0">
@@ -14,7 +14,7 @@ export default function Category({ category, index, kategori }) {
             src={icon === "" ? "/category-no.png" : icon}
             alt={name + "s image"}
             className={`
-            w-full h-full object-center object-cover duration-700 ease-in-out group-hover:opacity-75
+            w-full h-full object-center object-cover duration-700 ease-in-out group-hover:opacity-75 rounded-xl
             ${!icon
                 ? "scale-110 blur-2xl grayscale"
                 : "scale-100 blur-0 grayscale-0"
@@ -22,7 +22,7 @@ export default function Category({ category, index, kategori }) {
           `}
           />
         </span>
-        <span className="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50"></span>
+        <span className="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50 rounded-xl"></span>
         <span className="relative mt-auto sm:text-xl text-sm font-bold text-white w-[200px]">
           {name}
         </span>
