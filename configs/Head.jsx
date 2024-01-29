63c61ae1041be787d1acacb5e386c30bf1eb96cf
Benjamin <bfzli@hotmail.com>
2024-01-29 18:54:39 +0100

    @bfzli | Adding Metatags

--- configs/Head.jsx
+++ configs/Head.jsx
@@ -1,6 +1,5 @@
-import MetaTags from "next/head";
-
-import { Translation } from "../utils/Translations";
+import MetaTags from "next/head"
+import { Translation } from "../utils/Translations"
 
 export const Global = (props) => {
   const { 
@@ -17,12 +16,16 @@ export const Global = (props) => {
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
+
       <meta name="author" content={Translation("merrfal")} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <title>{title ? `${title} - ${Translation("merrfal")}` : Translation("merrfal")}</title>
+      <title>{title ? `${title?.replace('.', '')} - ${Translation("merrfal")}` : Translation("merrfal")}</title>
 
-      <meta property="og:title" content={`${title} - ${Translation("merrfal")}`} />
+      <meta property="og:title" content={`${title?.replace('.', '')} - ${Translation("merrfal")}`} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
       
