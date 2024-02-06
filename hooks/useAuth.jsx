c995e8f7a9a7d9bd92b577c79a7617040ca47bcc
Benjamin <bfzli@hotmail.com>
2024-02-07 00:37:24 +0100

    @bfzli | Fixes

--- hooks/useAuth.jsx
+++ hooks/useAuth.jsx
@@ -1,29 +1,29 @@
-import { Auth } from "../configs/Firebase";
-import { useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
-import { LogoutAccount } from "../controllers/Slices";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { Login } from "../api/User";
+import { Auth } from "../configs/Firebase"
+import { useDispatch } from "react-redux"
+import { useEffect, useState } from "react"
+import { LogoutAccount } from "../controllers/Slices"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { Login } from "../api/User"
 
 function useAuth(account) {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const [allow, setAllow] = useState(true);
-  const [user, loading] = useAuthState(Auth);
+  const [allow, setAllow] = useState(true)
+  const [user, loading] = useAuthState(Auth)
 
   useEffect(() => {
     if (!loading && allow) {
       if (!user && !account.Auth) {
-        setAllow(false);
-        dispatch(LogoutAccount());
+        setAllow(false)
+        dispatch(LogoutAccount())
       }
 
       if (user && !account.Auth) {
-        setAllow(false);
-        Login(user.uid, dispatch);
+        setAllow(false)
+        Login(user.uid, dispatch)
       }
     }
-  }, [user, loading]);
+  }, [user, loading])
 }
 
-export default useAuth;
+export default useAuth
