a06888bc4c73178d805a4d709ce6b769fea8bff7
Benjamin <bfzli@hotmail.com>
2024-02-09 18:57:32 +0100

    @bfzli | Fixes

--- middlewares/Cors.js
+++ middlewares/Cors.js
@@ -18,8 +18,6 @@ const Cors = async (req, res) => {
     else {
         const ORIGIN = req.headers['origin'] || req.headers['host']
 
-        console.log('ORIGIN', ORIGIN)
-
         if (ORIGINS.includes(ORIGIN)) {
             await cors(req, res, {
                 methods: '*',
