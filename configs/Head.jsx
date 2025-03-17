bcb4c78292322f26d59e67804b91d7b069b7cb8a
Benjamin <bfzli@hotmail.com>
2025-03-17 12:12:40 +0100

    @bfzli | adding some fixes

--- configs/Head.jsx
+++ configs/Head.jsx
@@ -1,20 +1,15 @@
 import MetaTags from "next/head"
 
 import { Translation } from "../utils/Translations"
-import { FB_PROJECT_ID } from "../configs/Envs"
 
 export const Global = (props) => {
   const { 
     title, 
     description = Translation("merrfal-description"), 
-    image = '/general-images/merrfal-hero.png',
-    index = true
+    image = '/general-images/merrfal-hero.png'
   } = props
 
-  let local_index = FB_PROJECT_ID.includes('local') ? true : index
   let formatted_title = (title === undefined || title?.includes?.('undefined')) ? Translation("merrfal") : `${title?.replace(/(^\w|\s\w)/g, (char) => char.toUpperCase())} - ${Translation("merrfal")}`
-
-  console.log('formatted_title', formatted_title)
 
   return (
     <MetaTags>
@@ -24,9 +19,7 @@ export const Global = (props) => {
 
       <meta property="og:title" content={formatted_title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-      
-      {local_index && <meta name="robots" content="noindex" />}
+      <meta property="og:image" content={image} />      
     </MetaTags>
   )
 }
--- ui/pages/Error/Error.jsx
+++ ui/pages/Error/Error.jsx
@@ -6,6 +6,8 @@ export default function Error(props) {
 
   return (
     <Normal>
+      <meta name="robots" content="noindex" />
+
       <div className="min-h-full pt-12 pb-32 flex flex-col bg-white">
         <div className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
           <div className="py-16">
